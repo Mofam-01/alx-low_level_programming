c3e388b7493decda685abc52d2473bd1ffd42855
Mofam-01 <finemypath@gmail.com>
2023-03-22 21:54:07 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,13 @@
 
 /**
  * _abs - Computes the absolute value of an integer.
- * @c: The number to be computed. 
+ * @c: The number to be computed.
  * Return: Absolute value of number or zero
  */
 
 int _abs(int c)
 
 {
-
 	if (c < 0)
 	{
 	int abs_val;
