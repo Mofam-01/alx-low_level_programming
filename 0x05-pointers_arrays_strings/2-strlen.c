0dcbfa21645f83a99e72af976d2b77d441062748
Mofam-01 <finemypath@gmail.com>
2023-03-27 21:37:07 -0700

    2-main.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,12 +8,9 @@
 
 int _strlen(char *s)
 {
-	int i;
-	int count = 0;
+	int string_length = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		count++;
-	}
-	return (count);
+	while (s[string_length])
+		string_length++;
+	return (string_lenth);
 }
